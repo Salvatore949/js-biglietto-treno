6d9d9f9844bf85fe402bf07885c639dc907730b5
Salvatore949 <salvo.mess49@gmail.com>
2021-10-21 16:57:50 +0200

    prezzo biglietto

--- JS/script.js
+++ JS/script.js
@@ -3,12 +3,16 @@
 const elemento = document.getElementById('mio_id');
 
 let km = prompt('Quanti km vuoi percorrere ?');
+ console.log(km);
 
 //..Chiedo l'età dell'utente.
 
 let age = prompt('Quanti anni hai ?');
+ console.log(age);
 
 //..Calcolo il prezzo del biblietto.
 
 let moltiplicazione = 0.21 * km;
  console.log(moltiplicazione);
+
+ //..Calcolo dello sconto.
