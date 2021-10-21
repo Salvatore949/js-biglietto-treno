1483b1fb1af64183c300afa7fa338820c5f836ea
Salvatore949 <salvo.mess49@gmail.com>
2021-10-21 17:55:21 +0200

    sconto

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,8 @@
 //..Chiedo all'utente quanti Km vuole percorrere.
 
-const elemento = document.getElementById('mio_id');
+let elemento = document.getElementById('mio_id');
 
-let km = prompt('Quanti km vuoi percorrere ?');
+const km = prompt('Quanti km vuoi percorrere ?');
  console.log(km);
 
 //..Chiedo l'età dell'utente.
@@ -16,3 +16,17 @@ let moltiplicazione = 0.21 * km;
  console.log(moltiplicazione);
 
  //..Calcolo dello sconto.
+
+ let ventipercento = (moltiplicazione * '0.2');
+  console.log(ventipercento);
+
+ let quarantapercento = (moltiplicazione * '0.4');
+  console.log(quarantapercento);
+
+ if(age<18){
+    elemento.innerHTML=`Costo biglietto: ${ventipercento}`;   
+}
+
+ if(age>65){
+    elemento.innerHTML=`Costo biglietto: ${quarantapercento}`;   
+}
