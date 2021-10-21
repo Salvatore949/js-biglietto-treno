b5ee3ff7c60655e8e7ae67f66c5ca001d300d6f2
Salvatore949 <salvo.mess49@gmail.com>
2021-10-21 16:53:28 +0200

    prezzo biglietto

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+//..Chiedo all'utente quanti Km vuole percorrere.
+
+const elemento = document.getElementById('mio_id');
+
+let km = prompt('Quanti km vuoi percorrere ?');
+
+//..Chiedo l'età dell'utente.
+
+let age = prompt('Quanti anni hai ?');
+
+//..Calcolo il prezzo del biblietto.
+
+let moltiplicazione = 0.21 * km;
+ console.log(moltiplicazione);
